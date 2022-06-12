c48c292d07a7e2a821ab1c9b1e9f449b42ae093f
MaxMccullough <mdm6ua@virginia.edu>
2022-06-12 10:19:35 -0400

    Intro Paragraph

--- portfolio-site/src/components/intro/Intro.jsx
+++ portfolio-site/src/components/intro/Intro.jsx
@@ -3,10 +3,15 @@ import './intro.scss'
 export default function Intro() {
   return (
     <div className='intro'>
-      <div className='topbarSpace'></div>
-      <div className="title">
-        Who is this Max guy?
-      
+      <div className="text">
+        <div className="title">
+          Who am I?
+        </div>
+        <br></br>
+        Hi there! My name is Max McCullough, and I am an undergraudate studying Computer Engineering at The University of Virginia.
+        I have created this website as a better way for potential employers to learn little more 
+        about me, so feel free to take a look around to learn about how I could make a positive 
+        impact at your company.
 
       </div>
 
--- portfolio-site/src/components/topbar/Topbar.jsx
+++ portfolio-site/src/components/topbar/Topbar.jsx
@@ -1,35 +1,24 @@
 import "./topbar.scss"
 import { Link } from "react-router-dom";
-import React, { useEffect, useState } from 'react'
 
-const Topbar = () => {
-  const [show, setShow] = useState(true)
-  const controlTopbar = () => {
-    if (window.scrollY > 100) {
-      setShow(false)
-    } else {
-      setShow(true)
-    }
-  }
-  useEffect(() => {
-    window.addEventListener('scroll', controlTopbar)
-    return () => {
-      window.removeEventListener('scroll', controlTopbar)
-    }
-  }, [])
+
+
+
+export default function Topbar() {
   return (
-    <div className={`nav ${show && 'topbar'}`}>
-      <div className="wrapper">
+    <div className="topbar">
+      <div className="wrapper">        
         <div className="left">
           <a href="/" className="logo">MAX</a>
           <div className="navItems">
-            <p><Link to="/intro" style={{ textDecoration: 'none', fontSize: '30px', color: 'white', fontWeight: '200' }}>INTRO&ensp;</Link></p>
-            <p><Link to="/contact" style={{ textDecoration: 'none', fontSize: '30px', color: 'white', fontWeight: '200' }}>CONTACT&ensp;</Link></p>
-            <p><Link to="/projects" style={{ textDecoration: 'none', fontSize: '30px', color: 'white', fontWeight: '200' }}>PROJECTS&ensp;</Link></p>
+            <p><Link to="/intro" style={{ textDecoration: 'none', fontSize: '30px', color: 'white', fontWeight: '300' }}>INTRO&ensp;</Link></p>
+            <p><Link to="/contact" style={{ textDecoration: 'none', fontSize: '30px', color: 'white', fontWeight: '300' }}>CONTACT&ensp;</Link></p>
+            <p><Link to="/projects" style={{ textDecoration: 'none', fontSize: '30px', color: 'white', fontWeight: '300' }}>PROJECTS&ensp;</Link></p>
+
+
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
-export default Topbar
