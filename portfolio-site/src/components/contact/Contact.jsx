c483467521ffae76af2349a8f2d4ac4f89e00809
MaxMccullough <mdm6ua@virginia.edu>
2022-06-20 20:40:18 -0400

    contact page

--- portfolio-site/src/components/contact/Contact.jsx
+++ portfolio-site/src/components/contact/Contact.jsx
@@ -1,8 +1,27 @@
 import "./contact.scss"
+//import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import { LinkedIn, Email, Phone } from '@mui/icons-material/';
 export default function Contact() {
     return (
-        <div className="contact">
-            This is my contact info
+        <div className='intro'>
+          <div className="text">
+            <div className="title">
+              Contact me here
+            </div>
+            I look forward to hearing from you!
+            <br/><br/>
+            <div className="icon"><div className="linkedIn"><LinkedIn size="large"/></div></div>
+            <a href="http://www.linkedin.com/in/max-mccullough-264baa1b5"><div className="link">linkedIn.com</div></a><br/>
+            <div className="icon"><div className="email"><Email/></div></div> 
+            <a href="max.mccullough.va@gmail.com"><div className="link">max.mccullough.va@gmail.com</div></a><br/>
+            <div className="icon"><div className="phone"><Phone/></div></div>
+            (571) 447-8476
+
+            
+            
+    
+          </div>
+    
         </div>
-    )
+      );
 }
